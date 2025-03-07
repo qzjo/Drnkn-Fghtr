e37d2dcf66b493aab24a68e78fa003179b45cac8
raweggyolk <edtso@s.sfusd.edu>
2025-03-07 11:24:43 -0800

    Pause Screen

---
+++ drnkn-fghtr/game.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var pause_screen: Control = $PauseScreen
+var paused = false
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		pauseScreen()
+
+
+func pauseScreen():
+	if paused:
+		pause_screen.hide()
+		Engine.time_scale = 1
+	else:
+		pause_screen.show()
+		Engine.time_scale = 0
+		
+	paused = !paused
