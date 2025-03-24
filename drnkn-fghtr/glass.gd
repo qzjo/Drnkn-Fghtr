fcd6c624505ceff62d194831e1974a91a0e813d9
qzjo <jaocampo@s.sfusd.edu>
2025-03-24 11:01:40 -0700

    added to inventory slot 1
    might be removed later on

--- drnkn-fghtr/glass.gd
+++ drnkn-fghtr/glass.gd
@@ -3,10 +3,13 @@ extends RigidBody2D
 @onready var character: Player = $"../Character"
 var inArea = false
 @onready var sprite_2d: Sprite2D = $"../Character/Sprite2D"
-var follow: Sprite2D
+@onready var lb1: Label = $"../Hotbar/Background/Organizer/Box1/Label"
+@onready var lb2: Label = $"../Hotbar/Background/Organizer/Box2/Label"
+@onready var lb3: Label = $"../Hotbar/Background/Organizer/Box3/Label"
+
 
 func _ready() -> void:
-	follow = sprite_2d
+	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	inArea = true # Replace with function body.
@@ -20,3 +23,8 @@ func _process(delta: float) -> void:
 	if inArea and Input.is_action_just_pressed("pick_up"):
 		print("Added " + name )
 		queue_free()
+
+		if lb1.text == "1":
+			lb1.text = sprite_2d.get_parent().name
+		elif lb1.text != sprite_2d.get_parent().name:
+			print("worked")
