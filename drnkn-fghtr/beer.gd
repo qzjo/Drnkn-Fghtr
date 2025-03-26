eeb33a6fdc1a5561dd17c6f22ec5e097ca04df40
raweggyolk <edtso@s.sfusd.edu>
2025-03-26 10:20:04 -0700

    Revert "beer pickup"
    This reverts commit 6d0a45c951a16e42ca7342f16058d6e86773ab26.

--- drnkn-fghtr/beer.gd
+++
@@ -1,7 +0,0 @@
-extends Item
-class_name beer
-
-@export var healing_amount := 100
-
-func use(player: Player) -> void:
-	player.health += healing_amount
--- drnkn-fghtr/item.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name Item
-
-@export var display_name := ""
-@export var texture: Texture2D = null
-@export var sound_on_pickup: AudioStream = null
-
-func use(player: Player) -> void:
-	pass
