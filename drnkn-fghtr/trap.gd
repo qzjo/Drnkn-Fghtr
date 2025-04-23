d72d1473283d2f3bcd00ef5a0b71e78f1524f3b8
raweggyolk <edtso@s.sfusd.edu>
2025-04-23 11:29:38 -0700

    trap

---
+++ drnkn-fghtr/trap.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+var bullet_path = preload("res://bullet.tscn")
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func fire():
+	var bullet = bullet_path.instantiate()
+	bullet.dir = $Node2D.global_rotation
+	bullet.pos = $Node2D.global_position
+	bullet.rota = global_rotation
+	get_parent().add_child(bullet)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		fire()
