06fa6bef1edf986781769ef59ac4e27eed786ced
raweggyolk <edtso@s.sfusd.edu>
2025-04-25 11:19:18 -0700

    bullets do damage but player cant die

--- drnkn-fghtr/bullet.gd
+++ drnkn-fghtr/bullet.gd
@@ -4,10 +4,17 @@ var pos: Vector2
 var rota:float
 var dir : float
 var speed = 2000
+var damage = 20
+@onready var bulletarea: Area2D = $Area2D
 
 func _ready():
 	global_position = pos
 	global_rotation = rota
+	bulletarea.body_entered.connect(func (body: Node) -> void:
+		if body is Player:
+			body.health -= damage
+			queue_free()
+			)
 
 func _physics_process(delta: float) -> void:
 	velocity=Vector2(speed, 0).rotated(dir)
