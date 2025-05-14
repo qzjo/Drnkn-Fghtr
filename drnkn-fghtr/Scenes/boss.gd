fe1da004b16494573590e0a542119b76d2d22d97
raweggyolk <edtso@s.sfusd.edu>
2025-05-14 10:47:36 -0700

    Revert "boss animation update"
    This reverts commit 725d6bdebd8d96a309dc7ba5a17222c3c682a103.

--- drnkn-fghtr/Scenes/boss.gd
+++ drnkn-fghtr/Scenes/boss.gd
@@ -33,10 +33,9 @@ func _physics_process(delta: float) -> void:
 			chase_player()
 		State.KNOCKBACK:
 			apply_knockback(delta)
-	update_animations()
 
 	move_and_slide()
-
+	update_animations()
 	
 	
 func take_damage(amount:int):
@@ -98,11 +97,10 @@ func _on_hitbox_body_entered(body: Node) -> void:
 
 	if body is Player:
 		var player := body as Player
-		animated_sprite_2d.play("Attack")
+		animated_sprite_2d.play("attack")
 		is_attacking = true
 		if not player.is_attacking():
 			player.take_damage()
-			player.knockback(position)
 		if player.is_attacking():
 			HitStopManager.hit_stop_short()
 			knockback()
